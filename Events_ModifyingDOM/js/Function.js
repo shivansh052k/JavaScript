ac46efe212f1449ea6402b0fd10890f1869b5260
Shivansh Gupta <shivanshgupta323@gmail.com>
2020-09-06 18:05:14 +0530

    Create Function.js

---
+++ Events_ModifyingDOM/js/Function.js
@@ -0,0 +1,9 @@
+function Click1()
+{
+	document.getElementById("Demo").innerHTML = "First Button Clicked";
+}
+
+function Click2()
+{
+	document.getElementById("Demo").innerHTML = "Second Button Clicked";
+}
